8090ee87858292c6cabdee6f043108648afdc440
Cüneyt Şentürk <cneytsenturk@gmail.com>
2025-04-10 10:18:58 +0100

    added public index.php

---
+++ public/index.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * @package     Akaunting
+ * @copyright   2017-2023 Akaunting. All rights reserved.
+ * @license     BSL; see LICENSE.txt
+ * @link        https://akaunting.com
+ */
+
+require __DIR__ . '/../index.php';
